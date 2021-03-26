cff8afcbb9d2099c2e39db8929b76f5bea280dc9
Semmy HAKIMI <hakimisemmy@gmail.com>
2021-03-26 15:11:51 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const accessToken =  async () =>{ await this.appPreferences.fetch("accessToken");
-console.log('111', accessToken);
-if(accessToken != null) {
-  if(this.tokenExpired(accessToken)){
-    console.log('222')
-    browser.executeScript({ code: "document.cookie = 'lpr5' + '=' + 'accessToken' + '; expires=' + 3 + '; domain=.laprovence.com; samesite=lax; secure; path=/ '" }).then()
-  }}
+const accessToken =  async () =>{ let test = await this.appPreferences.fetch("accessToken");
+console.log('111', test);
+// if(accessToken != null) {
+//   if(this.tokenExpired(accessToken)){
+//     console.log('222')
+//     browser.executeScript({ code: "document.cookie = 'lpr5' + '=' + 'accessToken' + '; expires=' + 3 + '; domain=.laprovence.com; samesite=lax; secure; path=/ '" }).then()
+//   }}
 }
